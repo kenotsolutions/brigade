ca96a7fd1bc1e1583e7d22bb87fe23e6f6946917
root <root@ip-172-20-63-56.ec2.internal>
2020-01-14 16:55:24 +0000

    sdfsdf

--- brigade.js
+++ brigade.js
@@ -4,6 +4,7 @@ const checkRunImage = "brigadecore/brigade-github-check-run:latest"
 events.on("check_suite:requested", checkRequested)
 events.on("check_suite:rerequested", checkRequested)
 events.on("check_run:rerequested", checkRequested)
+events.on("push", checkRequested)
 
 function checkRequested(e, p) {
   console.log("check requested")
