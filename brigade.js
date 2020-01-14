7bc5f6298f700fa601dce0f316045f16f7d5c988
root <root@ip-172-20-63-56.ec2.internal>
2020-01-14 16:39:38 +0000

    sdfsdf

--- brigade.js
+++ brigade.js
@@ -45,7 +45,7 @@ function checkRequested(e, p) {
     // In this case, we mark the ending failed.
     end.env.CHECK_CONCLUSION = "failure"
     end.env.CHECK_SUMMARY = "Build failed"
-    end.env.CHECK_TEXT = 
+    end.env.CHECK_TEXT = `Error: ${ err }`
     return end.run()
   })
 }
