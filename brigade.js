da3a05701738802a247e0253cfe45825b8b8dad1
root <root@ip-172-20-63-56.ec2.internal>
2020-01-15 01:34:51 +0000

    sdfsdf

--- brigade.js
+++ brigade.js
@@ -1,52 +1,22 @@
-const {events, Job, Group} = require("brigadier");
-const checkRunImage = "brigadecore/brigade-github-check-run:latest"
+const { events, Job, Group } = require('brigadier')
 
-events.on("check_suite:requested", checkRequested)
-events.on("check_suite:rerequested", checkRequested)
-events.on("check_run:rerequested", checkRequested)
-events.on("push", checkRequested)
+events.on("push", (brigadeEvent, project) => {
+    //variables
+    var gitPayload = JSON.parse(brigadeEvent.payload)
+    var today = new Date()
+    var gitSHA = brigadeEvent.revision.commit.substr(0,7)
 
-function checkRequested(e, p) {
-  console.log("check requested")
-  // Common configuration
-  const env = {
-    CHECK_PAYLOAD: e.payload,
-    CHECK_NAME: "MyService",
-    CHECK_TITLE: "Echo Test",
-  }
+    console.log("started")
+	
+    var acr = new Job("job-runner-acr-builder")
+   
+    console.log(gitPayload)
+    ]
 
-  // This will represent our build job. For us, it's just an empty thinger.
-  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo hello tg"])
 
-  // For convenience, we'll create three jobs: one for each GitHub Check
-  // stage.
-  const start = new Job("start-run", checkRunImage)
-  start.imageForcePull = true
-  start.env = env
-  start.env.CHECK_SUMMARY = "Beginning test run"
+    var pipeline = new Group()
 
-  const end = new Job("end-run", checkRunImage)
-  end.imageForcePull = true
-  end.env = env
-
-  // Now we run the jobs in order:
-  // - Notify GitHub of start
-  // - Run the test
-  // - Notify GitHub of completion
-  //
-  // On error, we catch the error and notify GitHub of a failure.
-  start.run().then(() => {
-    return build.run()
-  }).then( (result) => {
-    end.env.CHECK_CONCLUSION = "success"
-    end.env.CHECK_SUMMARY = "Build completed"
-    end.env.CHECK_TEXT = result.toString()
-    return end.run()
-  }).catch( (err) => {
-    // In this case, we mark the ending failed.
-    end.env.CHECK_CONCLUSION = "failure"
-    end.env.CHECK_SUMMARY = "Build failed"
-    end.env.CHECK_TEXT = `Error: ${ err }`
-    return end.run()
-  })
-}
+    pipeline.add(acr)
+    pipeline.runEach()
+    ] 
+})
