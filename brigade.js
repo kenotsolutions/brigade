2515b8838651a97f47c1698f4942f08abbd4180b
tolgay <tolgaygul@gmail.com>
2020-01-16 15:56:33 +0000

    test me tg

--- brigade.js
+++ brigade.js
@@ -1,11 +1,18 @@
 const { events, Job, Group } = require('brigadier')
 
 
-events.on("exec", () => {
+//events.on("exec", () => {
 
-	
+events.on("push", (brigadeEvent, project) => {	
 
 	console.log("==> handling an 'exec' eventisdfsdfsdfsdf")
+	var gitPayload = JSON.parse(brigadeEvent.payload)
+	var today = new Date()
+	var gitSHA = brigadeEvent.revision.commit.substr(0,7)
+         
+	const comment = payload.body.comment.body.trim();
+
+
 
 //	var acr = new Job("job-runner-acr-builder-tg")
 //	    acr.storage.enabled = false
@@ -19,12 +26,13 @@ events.on("exec", () => {
 
 	var helm = new Job("job-runner-helm")
 	helm.storage.enabled = false
-	helm.image = "lachlanevenson/k8s-helm:v2.9.1"
+	helm.serviceAccount = "brigade-installer";
+	helm.image = "lachlanevenson/k8s-helm:v3.0.2"
 	helm.tasks = [
 
-		          //  `helm upgrade --install graylog stable/graylog`
-		            `echo "helloword"`,
-		            `sleep 4800`
+		            `helm repo add stable "https://kubernetes-charts.storage.googleapis.com"`,
+		            `helm upgrade --install graylog stable/graylog`,
+		            `echo "comments:  " ${comment}`
 		        ]
 
 	var pipeline = new Group()
@@ -37,7 +45,7 @@ events.on("exec", () => {
 
 
 
-events.on("push", (brigadeEvent, project) => {
+events.on("pushit", (brigadeEvent, project) => {
     //variables
     var gitPayload = JSON.parse(brigadeEvent.payload)
     var today = new Date()
