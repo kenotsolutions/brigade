ec2ff5b1119ef4125c254053a2efc027e1f63231
root <root@ip-172-20-62-40.ec2.internal>
2020-01-20 00:14:42 +0000

    test

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
+const { events, Job } = require("@brigadecore/brigadier");
 const { KindJob } = require("@brigadecore/brigade-utils");
- 
+
 function e2e(event, project) {
-    let kind = new KindJob("kind");
-    kind.tasks.push(
-        // add your end-to-end tests
-        "kubectl get pods --all-namespaces"
-    );
- 
-    return kind;
+  let kind = new KindJob("kind");
+  kind.tasks.push(
+    // add your end-to-end tests
+    "kubectl get pods --all-namespaces"
+  );
+
+  return kind.run();
 }
- 
+
 events.on("exec", e2e);
