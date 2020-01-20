92034f2e57437a1f09aa7a8dbdd7fb7f6dc6ef2e
root <root@ip-172-20-62-40.ec2.internal>
2020-01-20 23:57:24 +0000

    test3

---
+++ test3.js
@@ -0,0 +1,29 @@
+const { events, Job } = require("brigadier")
+
+events.on("exec", (e, project) => {
+
+       console.log("==> handling an 'exec' eventisdfsdfsdfsdf")
+        var driver =  "overlay"
+
+// Build and push a Docker image.
+const docker = new Job("dind", "docker:stable-dind")
+docker.privileged = true;
+docker.env = {
+  DOCKER_DRIVER: driver
+}
+docker.env.DOCKER_USER = project.secrets.dockerLogin
+docker.env.DOCKER_PASS = project.secrets.dockerPass
+
+docker.tasks = [
+  "dockerd-entrypoint.sh &",
+  "sleep 20000",
+  "cd /src",
+  "docker build -t kenotsolutions/nginx-brigade:latest .", // Replace with your own image tag
+  "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+  "docker push kenotsolutions/nginx-brigade:latest" // Replace with your own image tag
+];
+
+docker.run()
+
+
+})
